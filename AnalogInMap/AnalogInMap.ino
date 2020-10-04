a1807be01c3122d01626ea64a61452dd36d370b8
Fabiano Riccardi <fabiano.riccardi@outlook.com>
2020-10-04 09:34:44 +0200

    Add Analog Read with map() conversion

---
+++ AnalogInMap/AnalogInMap.ino
@@ -0,0 +1,19 @@
+const int analogInPin = 6;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(5, OUTPUT);
+  digitalWrite(5, LOW);
+}
+
+void loop() {
+  int sensorValueRaw = analogRead(analogInPin);
+  int sensorValue = map(sensorValueRaw, 0, 8192, 0, 2600);
+
+  Serial.print("sensor = ");
+  Serial.print(sensorValueRaw);
+  Serial.print(" ");
+  Serial.println(sensorValue);
+
+  delay(300);
+}
