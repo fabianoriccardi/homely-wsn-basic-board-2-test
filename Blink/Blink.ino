66e92662372bfefb2836cdc7c3450a79618b4a20
Fabiano Riccardi <fabiano.riccardi@outlook.com>
2020-10-04 09:34:10 +0200

    add blick led test

---
+++ Blink/Blink.ino
@@ -0,0 +1,12 @@
+#define LED_BUILTIN 4
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000);                       
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
